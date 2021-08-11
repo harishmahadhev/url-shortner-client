838fd963664e12b1e256eb70291baa04bca74b9a
harishmahadhev <harishmahadhev@gmail.com>
2021-08-11 16:42:45 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 function App() {
   return (
-    <div className="App">
+    <Switch>
       <Home />
-    </div>
+    </Switch>
   );
 }
 
